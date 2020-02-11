8c49f96516be8b974eca4ba7fbbbe944de540b5f
duduluu <duduluu@outlook.com>
2020-02-11 22:44:01 +0800

    feat(config): type AssetCollection and postImport function

--- packages/config/src/exported.ts
+++ packages/config/src/exported.ts
@@ -28,3 +28,60 @@ export const TEMPLATES_EXPORTED: readonly Template[] = [
 
   'Product',
 ];
+
+export interface AssetCollection {
+  /**
+   * The array contains all of assets.
+   */
+  readonly list: readonly Asset[];
+
+  /**
+   * The map contains all of assets, key is guid.
+   */
+  readonly map: {
+    readonly [guid in number]?: Asset;
+  };
+
+  /**
+   * The map contains assets which has id (programming id, not guid).
+   */
+  readonly idMap: {
+    readonly [id in number]?: Asset;
+  };
+
+  /**
+   * Map, key is template name.
+   */
+  readonly templateMap: {
+    readonly [template in Template]?: readonly Asset[];
+  };
+}
+
+/**
+ * Process dist/assets.json after load
+ */
+export function postImport(assetsJson: Record<Template, Asset[]>): AssetCollection {
+  const list: Asset[] = [];
+  const map: Record<number, Asset> = {};
+  const idMap: Record<string, Asset> = {};
+
+  Object.entries(assetsJson).forEach(([template, assets]) =>
+    assets.forEach(asset => {
+      const { guid, id } = asset;
+      // eslint-disable-next-line no-param-reassign,@typescript-eslint/no-explicit-any
+      (asset as any).template = template;
+      list.push(asset);
+      map[guid] = asset;
+      if (id !== undefined) {
+        idMap[id] = asset;
+      }
+    }),
+  );
+
+  return {
+    list,
+    map,
+    idMap,
+    templateMap: assetsJson,
+  };
+}
